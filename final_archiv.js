6ea35bbef4f06fcb4e43f3a02fc9bc71dbd6ab78
DVP1209 <pateldevm1209@gmail.com>
2025-01-13 12:43:35 +0530

    Update background color for negative tags to a specific hex value

--- final_archiv.js
+++ final_archiv.js
@@ -999,7 +999,7 @@ function toggleTag(category, value) {
     });
     // Append the new tag to the container
     if(value.charAt(0) == "-"){
-      newTag.style.backgroundColor = "var(--peru) !important";
+      newTag.style.backgroundColor = "#bf8563 !important";
     }
     container.appendChild(newTag);
   }
